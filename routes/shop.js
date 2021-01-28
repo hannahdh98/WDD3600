fdd5583d599daa87cfa18e25461fded7c25e2b4d
hannahdh98 <56090227+hannahdh98@users.noreply.github.com>
2021-01-28 13:36:49 -0500

    Update shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
-
+// imports the path and express
 const path = require('path');
 const express = require('express');
+// gets the path from util
 const rootDir = require('../util/path');
+//creates the router
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
-
+//exports the router
 module.exports = router;
