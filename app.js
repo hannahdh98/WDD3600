c2ecf67c9287462549d4afb6caca6fd84a5e1f1c
hannahdh98 <ronnie98hdh@gmail.com>
2021-02-09 16:23:26 -0500

    updated app.js file

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-
-//import path, express, and the body parser
+//import path
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
 
 const app = express();
+
+//app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
+app.set('views', 'views')
+
+//const adminData = require('./routes/admin'); 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 //calls bodyParser to parse the body that's sent through the form
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));  
 
-app.use(express.static(path.join(__dirname, 'public', )));
-
-
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-//middleware to handle 404
-app.use((req, res, next) => {
-    //uses join method to connect to 404.html page 
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
-
+app.use(errorController.get404);
 // what port the server listens on. (localhost:3000)
 app.listen(3000);
